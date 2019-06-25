bfd7d1ca2ae14da6257c34fe6106cb4a48834174
이정현 <subacc09@daum.net>
2019-06-26 04:52:24 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react'
+import './App.css'
+
+const App: React.FC = () => {
+  return (
+    <Container>
+      <div className="todo-lists">
+        <div>
+          asdf
+        </div>
+        <div>
+          asdf
+        </div>
+        <div>
+          asdf
+        </div>
+      </div>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/LeftPane.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LeftPane: React.FC = () => {
+  return (
+    <>sup</>
+  )
+}
+
+export default LeftPane;
