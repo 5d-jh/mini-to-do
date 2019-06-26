5a7cc6ca46d486d79fb6dbf8653494479d84b8cc
이정현 <subacc09@daum.net>
2019-06-27 00:29:08 +0900

    Add press enter to submit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
 import React, { useState } from 'react';
 import ToDo from './ToDo';
-import { Button, Container, Input } from 'semantic-ui-react'
+import { Container, Input } from 'semantic-ui-react'
 import './App.css'
 
 const App: React.FC = () => {
   const [todoList, setTodoList]: [String[], Function] = useState([]);
   const [todoInput, setTodoInput]: [String, Function] = useState('');
 
+  const addTodo = (e: any): void => {
+    e.preventDefault();
+
+    if (todoInput.length !== 0) {
+      setTodoList([todoInput, ...todoList]);
+    }
+    
+    setTodoInput('');
+  }
+
   return (
     <Container>
-      <Input
-        action={ <Button onClick={ () => { setTodoList([todoInput, ...todoList]) } }>Add</Button> }
-        onChange={ ({ target: { value } }) => setTodoInput(value) }
-      />
+      <form onSubmit={addTodo}>
+        <Input
+          value={todoInput}
+          placeholder="Press Enter to add To-Do"
+          onChange={ ({ target: { value } }) => setTodoInput(value) }
+        />
+      </form>
       <div className="todo-lists">
-        { todoList.map(todo => <ToDo>{ todo }</ToDo> ) }
-      </div>
+          { todoList.map((todo, i) => <ToDo key={i}>{ todo }</ToDo> ) }
+        </div>
     </Container>
   );
 }
