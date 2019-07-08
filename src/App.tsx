a6d76873a88c7fafabdb6575151f5531db28c888
이정현 <subacc09@daum.net>
2019-07-09 04:06:58 +0900

    Improve design

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
           {
             selectedListInfo ? (
               <TodoList />
-            ) : null
+            ) : '👈 Select a to-do list to begin with.'
           }
         </RightPaneWrapper>
       </TodoContextProvider>
--- src/LeftPane.tsx
+++ src/LeftPane.tsx
@@ -4,7 +4,7 @@ import { SubTitle, TextInput } from './styles';
 import styled from 'styled-components';
 
 const LeftPane: React.FC<{ setSelectedListInfo: Function }> = ({ setSelectedListInfo }) => {
-  const { todoListInfos, todoListInfosDispatch } = useContext(TodoContext);
+  const { todoListInfos, todoListInfosDispatch, selectedListInfo } = useContext(TodoContext);
 
   const [ listNameInput, setListName ] = useState(String);
 
@@ -36,28 +36,36 @@ const LeftPane: React.FC<{ setSelectedListInfo: Function }> = ({ setSelectedList
           className="text-input"
         />
       </form>
-      <div className="group-list">
+      <ListWrapper>
         {
-          todoListInfos.map( todoList => (
-            <div key={`${todoList.todoListId}`}>
-              <button
-                id={`${todoList.todoListId}`}
-                name={`${todoList.listName}`}
-                key={`${todoList.todoListId}`}
-                onClick={ e => setSelectedListInfo({
-                  todoListId: Number(e.currentTarget.id),
-                  listName: e.currentTarget.name
+          todoListInfos.map( ({ todoListId, listName }) => (
+            <ListButtonWrapper key={`${todoListId}`}>
+              <ListColor />
+              <ListButton
+                id={`${todoListId}`}
+                name={`${listName}`}
+                key={`${todoListId}`}
+                isSelected={selectedListInfo && selectedListInfo.todoListId === todoListId}
+                onClick={ ({ currentTarget }) => setSelectedListInfo({
+                  todoListId: Number(currentTarget.id),
+                  listName: currentTarget.name
                 }) }
               >
-                {todoList.listName}
-              </button>
-            </div>
+                {listName}
+              </ListButton>
+            </ListButtonWrapper>
           ) )
         }
-      </div>
+      </ListWrapper>
     </>
   );
 }
+
+const ListWrapper = styled.div`
+  div + div {
+    margin-top: 3px;
+  }
+`;
 
 const UserTitle = styled.h2`
   margin: 0;
@@ -65,6 +73,45 @@ const UserTitle = styled.h2`
   font-size: 25px;
   font-weight: bold;
   color: #707070;
+`;
+
+const ListButtonWrapper = styled.div`
+  div ~ button {
+    margin-left: 9px;
+  }
+
+  display: flex;
+  flex-direction: row;
+  height: 35px;
+`;
+const ListColor = styled.div`
+  margin: auto 0;
+  background-color: #1B7CDD;
+  border: 3px solid #86BBF0;
+  width: 1.5em;
+  height: calc(1.5em - 2.7px);
+  border-radius: 50%;
+  box-sizing: border-box;
+`;
+const ListButton = styled.button<{ isSelected: Boolean | null }>`
+  :hover {
+    background-color: #F5F5F5;
+  }
+
+  cursor: pointer;
+
+  overflow-x: hidden;
+  margin: 0;
+  box-sizing: border-box;
+  padding: 4px 8px;
+  background-color: ${ props => props.isSelected ? '#F0F0F0' : 'white' };
+  font-weight: ${ props => props.isSelected ? 'bold' : 'medium' };
+  border: none;
+  width: 100%;
+  height: 100%;
+  text-align: left;
+  font-size: 20px;
+  border-radius: 8px;
 `;
 
 export default LeftPane;
